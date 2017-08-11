f62b3080d446f3d076ed9ff93ce2cc9a3ef7fec3
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-11 13:10:30 -0700

    add bug fixes

--- SonOfCod.tests/ControllerTests/AccountControllerTests.cs
+++ SonOfCod.tests/ControllerTests/AccountControllerTests.cs
@@ -2,15 +2,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Xunit;
 using SonOfCodWebsite.Controllers;
+using SonOfCodWebsite.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace SonOfCod.tests.ControllerTests
 {
     public class AccountControllerTests
     {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManger;
+
+        [Fact]
+        public void Get_ViewResult_Index_Home_Test()
+        {
+            HomeController controller = new HomeController();
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+
         [Fact]
         public void Get_ViewResult_Index_Test()
         {
-            AccountController controller = new AccountController();
+            AccountController controller = new AccountController(_userManager, _signInManger, _db);
             var result = controller.Index();
             Assert.IsType<ViewResult>(result);
         }
@@ -18,9 +32,9 @@ namespace SonOfCod.tests.ControllerTests
         [Fact]
         public void Get_ModelList_Index_Test()
         {
-            AccountController controller = new AccountController();
+            AccountController controller = new AccountController(_userManager, _signInManger, _db);
             IActionResult actionResult = controller.Index();
-            ViewResult indexView = new AccountController().Index() as ViewResult;
+            ViewResult indexView = actionResult as ViewResult;
             var result = indexView.ViewData.Model;
             Assert.IsType<List<AccountControllerTests>>(result);
         }
@@ -28,7 +42,7 @@ namespace SonOfCod.tests.ControllerTests
         [Fact]
         public void Get_ViewResult_Create_Test()
         {
-            AccountController controller = new AccountController();
+            AccountController controller = new AccountController(_userManager, _signInManger, _db);
             var result = controller.Create();
             Assert.IsType<ViewResult>(result);
         }
