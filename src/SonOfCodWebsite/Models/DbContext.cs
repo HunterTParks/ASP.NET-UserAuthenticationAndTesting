3c2f63a416725aa752440377619d998bbd9d9992
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-11 08:53:12 -0700

    add class that extends the IdentityDbContext class to our project

---
+++ src/SonOfCodWebsite/Models/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SonOfCodWebsite.Models
+{
+    public class DbContext : IdentityDbContext<User>
+    {
+        public DbContext(DbContextOptions options) : base(options)
+    }
+}
