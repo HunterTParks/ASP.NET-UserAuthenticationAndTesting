81fc6676cf9c9ca752f5284901391c4fc40145ab
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-11 10:17:05 -0700

    add tests to the Account Controller testing class and correctly reference the tests to the working solution

--- SonOfCod.tests/ControllerTests/AccountControllerTests.cs
+++ SonOfCod.tests/ControllerTests/AccountControllerTests.cs
@@ -1,6 +1,19 @@
-﻿namespace SonOfCod.tests.ControllerTests
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCodWebsite.Controllers;
+using SonOfCodWebsite.Models;
+using Xunit;
+
+namespace SonOfCod.tests.ControllerTests
 {
     public class AccountControllerTests
     {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            AccountController controller = new AccountController();
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
     }
 }
