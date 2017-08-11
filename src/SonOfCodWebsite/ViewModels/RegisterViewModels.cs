70e1876df1de3191ec3e2a8f8d1aad43ee844c51
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-11 09:55:26 -0700

    add View Model folder and added a View Model for Registering a User

---
+++ src/SonOfCodWebsite/ViewModels/RegisterViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SonOfCodWebsite.ViewModels
+{
+    public class RegisterViewModels
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The confirmation password does not match the first password.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
