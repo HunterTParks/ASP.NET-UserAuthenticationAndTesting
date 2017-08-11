9ee7e1e10f0ce49ef30850f60f7d7847ae2069cc
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-11 09:37:07 -0700

    add properties and a constructor to our Account Controller

--- src/SonOfCodWebsite/Controllers/AccountController.cs
+++ src/SonOfCodWebsite/Controllers/AccountController.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SonOfCodWebsite.Models;
 
 namespace SonOfCodWebsite.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: /<controller>/
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManger;
+
+        public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManger = signInManager;
+            _db = db;
+        }
+        
         public IActionResult Index()
         {
             return View();
