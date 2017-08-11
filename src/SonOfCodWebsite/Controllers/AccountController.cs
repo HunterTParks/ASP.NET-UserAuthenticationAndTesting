ccc3c690db556671f80b2f28afce4de746cd0570
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-11 14:18:25 -0700

    add login template that will be used for the administrator

--- src/SonOfCodWebsite/Controllers/AccountController.cs
+++ src/SonOfCodWebsite/Controllers/AccountController.cs
@@ -44,5 +44,10 @@ namespace SonOfCodWebsite.Controllers
                 return View();
             }
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
